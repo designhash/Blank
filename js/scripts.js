0ca3b37a23978cc91e419a3a4c1f9e984ae45939
Don Gepulango <dongepulango@gmail.com>
2016-07-13 17:50:28 +0800

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,16 @@ $(function () {
     $(this).toggleClass("active");
   }); 
 });
+
+$(function () {
+  $('.carousel').carousel({
+    interval: 0
+  }).on('slide.bs.carousel', function (e) {
+    var nextH = $(e.relatedTarget).height();
+    console.log(nextH)
+    console.log( $(this).find('.active.item').parent() )
+    $(this).find('.active.item').parent().animate({
+        height: nextH
+    }, 500);
+  });
+});
